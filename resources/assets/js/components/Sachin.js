58dabed028f4588056434a8cd6879b87c9521224
Developer <sachinsingh5891@gmail.com>
2019-04-13 13:08:41 +0530

    push all

---
+++ resources/assets/js/components/Sachin.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Sachin extends Component {
+    render() {
+        return (
+            <div className="sddddachin">
+            <h1>My name is Sachin Singh</h1>
+            </div>
+        );
+    }
+}
+
+export default Sachin;
